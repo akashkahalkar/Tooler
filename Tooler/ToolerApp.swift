15581baa7fae4576e067c00e0276ebedea725db3
Akash Kahalkar <akash.kahalkar.08@gmail.com>
2023-12-26 11:04:50 +0530

    Initial Commit

---
+++ Tooler/ToolerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToolerApp.swift
+//  Tooler
+//
+//  Created by akash kahalkar on 26/12/23.
+//
+
+import SwiftUI
+
+@main
+struct ToolerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
